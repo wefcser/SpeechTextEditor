fc83b06dc690ccb52995fdd09e1729c1d7f62953
wefcser <2432963477@qq.com>
2017-05-09 22:00:02 +0800

    add core data

--- SpeechTextEditor/ListViewController.swift
+++ SpeechTextEditor/ListViewController.swift
@@ -7,24 +7,64 @@
 //
 
 import UIKit
+import CoreData
+
+let app:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+let context:NSManagedObjectContext = app.persistentContainer.viewContext
 
 class ListViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
 
-    var items:[String] = ["武汉","上海","北京","深圳","广州","重庆","香港","台海","天津","南京","安徽","长沙","云南","台北"]
-    
+    var items:[Date:String?] = [:]
+    var keys:[Date] = []
     @IBOutlet weak var textList: UITableView!
     
     override func viewDidLoad() {
+        print("viewDidLoad begin")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.textList.delegate=self
         self.textList.dataSource=self
-
+        
+        let fetchRequest = NSFetchRequest<Text>(entityName:"Text")
+        //let predicate = NSPredicate(format: "", "")
+        //fetchRequest.predicate = predicate
+        
+        do {
+            let fetchedObjects = try context.fetch(fetchRequest)
+            
+            //遍历查询的结果
+            for text in fetchedObjects{
+                let index = text.content?.index((text.content?.startIndex)!, offsetBy: 5)
+                self.keys.append(text.date! as Date)
+                self.items[text.date! as Date]=text.content?.substring(to: index!)
+            }
+            self.keys.sort()
+        }
+        catch {
+            fatalError("不能保存：\(error)")
+        }
+        
+        print("viewDidLoad end")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier=="showTextView"){
+            let dest=segue.destination as! TextViewController
+            dest.navigationItem.title="textView"
+            dest.textIndex=3
+        }
+        print("prepare")
+    }
+    
+    @IBAction func goTextView(_ sender: UIButton) {
+        print("go")
+        self.performSegue(withIdentifier: "showTextView", sender: self)
+        print("goback")
     }
     //返回几组
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -40,19 +80,25 @@ class ListViewController: UIViewController,UITableViewDelegate, UITableViewDataS
     }
     //每一行内容
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier: String = "cellIdentifier"
+        print("cell begin")
+        let cellIdentifier: String = "cellModel1"
         var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
         if(cell==nil){
+            print("cell nil")
             cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: cellIdentifier)
             cell!.textLabel?.font = UIFont.systemFont(ofSize: 14)
             cell!.selectionStyle = .gray
             cell!.accessoryType = UITableViewCellAccessoryType.none
         }
-        cell!.textLabel?.text = self.items[indexPath.row]
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = formatter.string(from: keys[indexPath.row] as Date)
+        cell!.textLabel?.text=dateString
+        //cell!.contentView.
         //cell!.imageView?.image = UIImage(named:"cellImage.png")
         //cell!.detailTextLabel?.text = "详细信息介绍"
         
-        
+        print("cell end")
         return cell!
     }
     //点击一行
@@ -78,16 +124,16 @@ class ListViewController: UIViewController,UITableViewDelegate, UITableViewDataS
     }*/
     //
     //自定义编辑操作,自定义按钮样式
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        let deleteAction:UITableViewRowAction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "删除", handler: {(action:UITableViewRowAction,indexPath: IndexPath) in
-            let index=indexPath.row as Int
-            self.items.remove(at: index)
-            self.textList.deleteRows(at: [indexPath], with: UITableViewRowAnimation.top)
-            NSLog("删除\(indexPath.row)")
-        })
-        //deleteAction.backgroundColor = UIColor.green
-        deleteAction.backgroundEffect = UIBlurEffect(style:UIBlurEffectStyle.dark)
-        return [deleteAction]
-    }
+//    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+////        let deleteAction:UITableViewRowAction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "删除", handler: {(action:UITableViewRowAction,indexPath: IndexPath) in
+////            let index=indexPath.row as Int
+////            self.items.remove(at: index)
+////            self.textList.deleteRows(at: [indexPath], with: UITableViewRowAnimation.top)
+////            NSLog("删除\(indexPath.row)")
+//        })
+//        //deleteAction.backgroundColor = UIColor.green
+//        deleteAction.backgroundEffect = UIBlurEffect(style:UIBlurEffectStyle.dark)
+//        return [deleteAction]
+//    }
 }
 
--- SpeechTextEditor/TextViewController.swift
+++ SpeechTextEditor/TextViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
-class TextViewController: UIViewController {
+class TextViewController: UIViewController,UITextViewDelegate{
+    var textIndex:Int?
+    
+    @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.textView.delegate=self
+        
+        self.textView.backgroundColor = UIColor.clear
+        self.textView.textAlignment = NSTextAlignment.left
+        self.textView.textColor = UIColor.darkText
+        self.textView.font = UIFont(name: "GillSans", size: 20.0)
+        
+        self.textView.isEditable = true
+        self.textView.isUserInteractionEnabled = true
+        self.textView.isScrollEnabled = true
+        
+        //self.app = UIApplication.shared.delegate as! AppDelegate
+        //self.context = app.persistentContainer.viewContext
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,12 +37,54 @@ class TextViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBOutlet weak var text: UITextView!
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+        print(self.textIndex!)
+        self.textView.text="wefcser王乙飞"
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        print("textViewShouldBeginEditing")
+        return true
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print("textViewDidBeginEditing")
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        print("textViewDidChange")
+    }
+    
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        print("textViewShouldEndEditing")
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print("textViewDidEndEditing")
+    }
     
     @IBAction func back(_ sender: UIBarButtonItem) {
+        print(self.navigationItem.title!)
         self.dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func save(_ sender: UIBarButtonItem) {
+        let text = NSEntityDescription.insertNewObject(forEntityName: "Text", into: context) as! Text
+        text.date=NSDate()
+        text.content=self.textView.text
         
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = formatter.string(from: text.date! as Date)
+        print(dateString)
+        
+        do {
+            try context.save()
+            print("保存成功！")
+        } catch {
+            fatalError("不能保存：\(error)")
+        }
     }
 }
