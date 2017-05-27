d09adb648277565a00b8d7340f5f3294f8000d8a
wefcser <2432963477@qq.com>
2017-05-27 11:20:54 +0800

    add CustomCell.swift

---
+++ SpeechTextEditor/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  SpeechTextEditor
+//
+//  Created by 王乙飞 on 2017/5/27.
+//  Copyright © 2017年 王乙飞. All rights reserved.
+//
+
+import UIKit
+
+class ListCell:UITableViewCell{
+    
+    @IBOutlet weak var date: UILabel!
+    
+    @IBOutlet weak var content: UILabel!
+}
